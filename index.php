03dec47c3b47b9df64d386830b045045bebd44de
Mrinal Bhambhu <mrinalbhambhu@Mrinals-MacBook-Air.local>
2017-01-28 17:22:35 +0000

    home page structure

---
+++ index.php
@@ -0,0 +1,265 @@
+<?php
+//set default time zone
+date_default_timezone_set('UTC');
+
+
+
+//echo date('jS');
+//echo date('l');
+?>
+<html>
+<head>
+	<title>Booking Calendar</title>
+	<link rel="stylesheet" type="text/css" href="assets/style.css">
+	<link href="https://fonts.googleapis.com/css?family=Anton|Titillium+Web" rel="stylesheet">
+	
+</head>
+<body>
+
+	<div class="main-container">
+		
+			<div class="top-cont">
+				<div class="col-30"><div class="month-dis">January 2017</div></div>
+				<div class="col-30"><div class="arrow">&lt;</div><div class="arrow">&gt;</div></div>
+				<div class="col-30">
+					Select Source:
+					<select>
+					  <option value="volvo">Meeting room</option>
+					  <option value="saab">Table tennis</option>
+					  <option value="opel">Pool table</option>
+					  <option value="audi">Gym class</option>
+					</select>
+				</div>
+
+			</div>
+
+			<div class="calendar-con">
+
+				<div class="cal-hd">Monday</div>
+				<div class="cal-hd">Tuesday</div>
+				<div class="cal-hd">Wednesday</div>
+				<div class="cal-hd">Thursday</div>
+				<div class="cal-hd">Friday</div>
+				<div class="cal-hd">Saturday</div>
+				<div class="cal-hd">Sunday</div>
+
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				<div class="calendar-dv">
+					<div class="date-dv">1</div>
+					<div class="avl-book-dv">Book now+</div>
+					<div class="avl-num-dv">Avialable: 10</div>
+				</div>
+				
+
+			</div>
+
+		<div class= "admin-pnl"><a href="admin.php">Go to admin panel</a></div>
+
+	</div>
+	
+</body>
+</html>
